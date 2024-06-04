587d7f6c74de365b92e69952bc6527e5450efb84
Wenqiang Sun <93043187+wenqsun@users.noreply.github.com>
2024-06-04 17:05:31 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,125 +1,160 @@
-import numpy as np
 import torch
-import time
-import nvdiffrast.torch as dr
-from util.utils import get_tri
-import tempfile
-from mesh import Mesh
-import zipfile
-import cv2
-def generate3d(model, rgb, ccm, freq_modulate, freq_type, kernel_size, device):
+from libs.base_utils import do_resize_content
+from imagedream.ldm.util import (
+    instantiate_from_config,
+    get_obj_from_str,
+)
+from omegaconf import OmegaConf
+from PIL import Image
+import numpy as np
+from inference import generate3d
+from huggingface_hub import hf_hub_download
+import json
+import argparse
+import shutil
+from model import CRM
+import PIL
+import rembg
+import os
+from pipelines import TwoStagePipeline
 
-    color_tri = torch.from_numpy(rgb)/255
-    xyz_tri = torch.from_numpy(ccm[:,:,(2,1,0)])/255
-    color = color_tri.permute(2,0,1)
-    xyz = xyz_tri.permute(2,0,1)
+rembg_session = rembg.new_session()
 
+def expand_to_square(image, bg_color=(0, 0, 0, 0)):
+    # expand image to 1:1
+    width, height = image.size
+    if width == height:
+        return image
+    new_size = (max(width, height), max(width, height))
+    new_image = Image.new("RGBA", new_size, bg_color)
+    paste_position = ((new_size[0] - width) // 2, (new_size[1] - height) // 2)
+    new_image.paste(image, paste_position)
+    return new_image
 
-    def get_imgs(color):
-        # color : [C, H, W*6]
-        color_list = []
-        color_list.append(color[:,:,256*5:256*(1+5)])
-        for i in range(0,5):
-            color_list.append(color[:,:,256*i:256*(1+i)])
-        return torch.stack(color_list, dim=0)# [6, C, H, W]
-    
-    triplane_color = get_imgs(color).permute(0,2,3,1).unsqueeze(0).to(device)# [1, 6, H, W, C]
+def remove_background(
+    image: PIL.Image.Image,
+    rembg_session = None,
+    force: bool = False,
+    **rembg_kwargs,
+) -> PIL.Image.Image:
+    do_remove = True
+    if image.mode == "RGBA" and image.getextrema()[3][0] < 255:
+        # explain why current do not rm bg
+        print("alhpa channl not enpty, skip remove background, using alpha channel as mask")
+        background = Image.new("RGBA", image.size, (0, 0, 0, 0))
+        image = Image.alpha_composite(background, image)
+        do_remove = False
+    do_remove = do_remove or force
+    if do_remove:
+        image = rembg.remove(image, session=rembg_session, **rembg_kwargs)
+    return image
 
-    color = get_imgs(color)
-    xyz = get_imgs(xyz)
-
-    color = get_tri(color, dim=0, blender= True, scale = 1).unsqueeze(0)
-    xyz = get_tri(xyz, dim=0, blender= True, scale = 1, fix= True).unsqueeze(0)
-
-    triplane = torch.cat([color,xyz],dim=1).to(device)
-    # 3D visualize
-    model.eval()
-    glctx = dr.RasterizeCudaContext()
-
-    if model.denoising == True:
-        tnew = 20
-        tnew = torch.randint(tnew, tnew+1, [triplane.shape[0]], dtype=torch.long, device=triplane.device)
-        noise_new = torch.randn_like(triplane) *0.5+0.5
-        triplane = model.scheduler.add_noise(triplane, noise_new, tnew)    
-        start_time = time.time()
-        with torch.no_grad():
-            triplane_feature2 = model.unet2(triplane,tnew)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(f"unet takes {elapsed_time}s")
+def do_resize_content(original_image: Image, scale_rate):
+    # resize image content wile retain the original image size
+    if scale_rate != 1:
+        # Calculate the new size after rescaling
+        new_size = tuple(int(dim * scale_rate) for dim in original_image.size)
+        # Resize the image while maintaining the aspect ratio
+        resized_image = original_image.resize(new_size)
+        # Create a new image with the original size and black background
+        padded_image = Image.new("RGBA", original_image.size, (0, 0, 0, 0))
+        paste_position = ((original_image.width - resized_image.width) // 2, (original_image.height - resized_image.height) // 2)
+        padded_image.paste(resized_image, paste_position)
+        return padded_image
     else:
-        triplane_feature2 = model.unet2(triplane)
-        
-    original_triplane = triplane_feature2
-    if freq_modulate:
-        triplane_feature2 = fre_modulate(triplane_feature2, freq_type, kernel_size)
-        triplane_feature2 = torch.from_numpy(triplane_feature2).unsqueeze(0).to(device)
+        return original_image
 
-    with torch.no_grad():
-        data_config = {
-            'resolution': [1024, 1024],
-            "triview_color": triplane_color.to(device),
-        }
+def add_background(image, bg_color=(255, 255, 255)):
+    # given an RGBA image, alpha channel is used as mask to add background color
+    background = Image.new("RGBA", image.size, bg_color)
+    return Image.alpha_composite(background, image)
 
-        verts, faces = model.decode(data_config, triplane_feature2)
 
-        data_config['verts'] = verts[0]
-        data_config['faces'] = faces
-        
-
-    from kiui.mesh_utils import clean_mesh
-    verts, faces = clean_mesh(data_config['verts'].squeeze().cpu().numpy().astype(np.float32), data_config['faces'].squeeze().cpu().numpy().astype(np.int32), repair = False, remesh=False, remesh_size=0.005)
-    data_config['verts'] = torch.from_numpy(verts).cuda().contiguous()
-    data_config['faces'] = torch.from_numpy(faces).cuda().contiguous()
-
-    start_time = time.time()
-    with torch.no_grad():
-        mesh_path_obj = tempfile.NamedTemporaryFile(suffix=f"", delete=False).name
-        model.export_mesh_wt_uv(glctx, data_config, mesh_path_obj, "", device, res=(1024,1024), tri_fea_2=original_triplane)
-
-        mesh = Mesh.load(mesh_path_obj+".obj", bound=0.9, front_dir="+z")
-        mesh_path_glb = tempfile.NamedTemporaryFile(suffix=f"", delete=False).name
-        mesh.write(mesh_path_glb+".glb")
-
-        # mesh_obj2 = trimesh.load(mesh_path_glb+".glb", file_type='glb')
-        # mesh_path_obj2 = tempfile.NamedTemporaryFile(suffix=f"", delete=False).name
-        # mesh_obj2.export(mesh_path_obj2+".obj")
-
-        with zipfile.ZipFile(mesh_path_obj+'.zip', 'w') as myzip:
-            myzip.write(mesh_path_obj+'.obj', mesh_path_obj.split("/")[-1]+'.obj')
-            myzip.write(mesh_path_obj+'.png', mesh_path_obj.split("/")[-1]+'.png')
-            myzip.write(mesh_path_obj+'.mtl', mesh_path_obj.split("/")[-1]+'.mtl')
-
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print(f"uv takes {elapsed_time}s")
-    return mesh_path_glb+".glb", mesh_path_obj+'.zip'
-
-def fre_modulate(triplanes, freq_type="bilateralFilter", kernel_size=9):
+def preprocess_image(image, background_choice, foreground_ratio, backgroud_color):
     """
-    Input: [1, 32, 256, 768]
+    input image is a pil image in RGBA, return RGB image
     """
-    triplanes = triplanes.cpu().numpy()
-    triplanes = triplanes[0]      # [32, 256, 768]
-    triplane_list = []
-    for i in range(3):
-        triplane_list.append(triplanes[:,:,256*i:256*(i+1)])
-    triplane = np.stack(triplane_list, axis=0)
-    triplane = np.reshape(triplane, (3, 32, 256, 256))
-    print(triplane.shape)
-    for i in range(triplane.shape[0]):
-        sub_triplane = triplane[i]  # 32, 256, 256
-        for j in range(sub_triplane.shape[0]):
-            img = sub_triplane[j]
-            if freq_type == "GaussianBlur":
-                img = cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
-            elif freq_type == "bilateralFilter":
-                img = img.astype(np.float32)
-                img = cv2.bilateralFilter(img, kernel_size, 75, 75)
-            sub_triplane[j] = img
-        triplane[i] = sub_triplane
+    print(background_choice)
+    if background_choice == "Alpha as mask":
+        background = Image.new("RGBA", image.size, (0, 0, 0, 0))
+        image = Image.alpha_composite(background, image)
+    else:
+        image = remove_background(image, rembg_session, force_remove=True)
+    image = do_resize_content(image, foreground_ratio)
+    image = expand_to_square(image)
+    image = add_background(image, backgroud_color)
+    return image.convert("RGB")
 
-    for i in range(3):
-        triplanes[:, :, 256*i:256*(i+1)] = triplane[i]
+if __name__ == "__main__":
 
-    return triplanes
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--inputdir",
+        type=str,
+        default="examples/kunkun.webp",
+        help="dir for input image",
+    )
+    parser.add_argument(
+        "--scale",
+        type=float,
+        default=5.0,
+    )
+    parser.add_argument(
+        "--step",
+        type=int,
+        default=50,
+    )
+    parser.add_argument(
+        "--bg_choice",
+        type=str,
+        default="Auto Remove background",
+        help="[Auto Remove background] or [Alpha as mask]",
+    )
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        default="out/",
+    )    
+    args = parser.parse_args()
+    
+
+    img = Image.open(args.inputdir)
+    img = preprocess_image(img, args.bg_choice, 1.0, (127, 127, 127))
+    os.makedirs(args.outdir, exist_ok=True)
+    img.save(args.outdir+"preprocessed_image.png")
+
+    crm_path = hf_hub_download(repo_id="Zhengyi/CRM", filename="CRM.pth")
+    specs = json.load(open("configs/specs_objaverse_total.json"))
+    model = CRM(specs).to("cuda")
+    model.load_state_dict(torch.load(crm_path, map_location = "cuda"), strict=False)
+
+    stage1_config = OmegaConf.load("configs/nf7_v3_SNR_rd_size_stroke.yaml").config
+    stage2_config = OmegaConf.load("configs/stage2-v2-snr.yaml").config
+    stage2_sampler_config = stage2_config.sampler
+    stage1_sampler_config = stage1_config.sampler
+
+    stage1_model_config = stage1_config.models
+    stage2_model_config = stage2_config.models
+
+    xyz_path = hf_hub_download(repo_id="Zhengyi/CRM", filename="ccm-diffusion.pth")
+    pixel_path = hf_hub_download(repo_id="Zhengyi/CRM", filename="pixel-diffusion.pth")
+    stage1_model_config.resume = pixel_path
+    stage2_model_config.resume = xyz_path
+
+    pipeline = TwoStagePipeline(
+        stage1_model_config,
+        stage2_model_config,
+        stage1_sampler_config,
+        stage2_sampler_config,
+    )
+
+    rt_dict = pipeline(img, scale=args.scale, step=args.step)
+    stage1_images = rt_dict["stage1_images"]
+    stage2_images = rt_dict["stage2_images"]
+    np_imgs = np.concatenate(stage1_images, 1)
+    np_xyzs = np.concatenate(stage2_images, 1)
+    Image.fromarray(np_imgs).save(args.outdir+"pixel_images.png")
+    Image.fromarray(np_xyzs).save(args.outdir+"xyz_images.png")
+
+    glb_path, obj_path = generate3d(model, np_imgs, np_xyzs, "cuda")
+    shutil.copy(obj_path, args.outdir+"output3d.zip")
